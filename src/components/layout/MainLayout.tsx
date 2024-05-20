c67c77f20da41aa1eb700326ee812c3b541a7c8e
jarvinc3 <mangomc03@gmail.com>
2024-05-20 12:24:18 -0400

    ?

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -11,22 +11,45 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
 
   const handleClick = () => {
     setOpen(!open);
-  }
+  };
 
   return (
     <div className="fixed sm:relative flex flex-col justify-between sm:flex-row sm:justify-center sm:items-start w-screen h-screen sm:gap-10 sm:pt-[10%] p-4 sm:p-0 main">
       <section className="sm:w-[20%] hidden mt-28 sm:mt-0 bg-white rounded-2xl sm:flex justify-center px-8">
         <ProfileInfo />
       </section>
-      {!open && <section className="p-5 overflow-y-auto bg-white sm:max-h-[90%] max-h-[75%] sm:p-16 sm:w-1/2 rounded-2xl scrollbar-hide relative">{children}</section>}
+      {!open && (
+        <section className="p-4 overflow-hidden overflow-y-scroll bg-white sm:max-h-[90%] max-h-[75%] sm:p-16 sm:w-1/2 rounded-2xl relative">
+          {children}
+        </section>
+      )}
       <section className="sm:w-[7%] w-[44vh] sm:h-auto z-10 h-[70px] bg-white rounded-2xl p-3 py-4 flex justify-center mt-3 sm:mt-0 fixed sm:relative bottom-3 sm:bottom-0 sm:left-0 sm:translate-x-0 left-1/2 -translate-x-1/2">
         <Navbar open={open} onClick={handleClick} />
       </section>
-      {open && <div className="absolute h-[73%] w-[44vh] px-10 transform -translate-x-1/2 bg-white cursor-pointer rounded-2xl top-3 sm:hidden left-1/2 overflow-y-auto scrollbar-hide"><ProfileInfo /></div>}
-      {!open
-        ? <img onClick={handleClick} className="fixed transform -translate-x-1/2 cursor-pointer opacity-30 bottom-20 sm:hidden left-1/2" width="50" height="50" src="https://img.icons8.com/ios-filled/50/collapse-arrow.png" alt="collapse-arrow" />
-        : <img onClick={handleClick} className="fixed transform -translate-x-1/2 cursor-pointer bottom-20 sm:hidden left-1/2" width="50" height="50" src="https://img.icons8.com/ios-filled/50/expand-arrow--v1.png" alt="expand-arrow--v1" />
-      }
+      {open && (
+        <div className="absolute h-[73%] w-[44vh] px-10 transform -translate-x-1/2 bg-white cursor-pointer rounded-2xl top-3 sm:hidden left-1/2 overflow-y-auto scrollbar-hide">
+          <ProfileInfo />
+        </div>
+      )}
+      {!open ? (
+        <img
+          onClick={handleClick}
+          className="fixed transform -translate-x-1/2 cursor-pointer opacity-30 bottom-20 sm:hidden left-1/2"
+          width="50"
+          height="50"
+          src="https://img.icons8.com/ios-filled/50/collapse-arrow.png"
+          alt="collapse-arrow"
+        />
+      ) : (
+        <img
+          onClick={handleClick}
+          className="fixed transform -translate-x-1/2 cursor-pointer bottom-20 sm:hidden left-1/2"
+          width="50"
+          height="50"
+          src="https://img.icons8.com/ios-filled/50/expand-arrow--v1.png"
+          alt="expand-arrow--v1"
+        />
+      )}
     </div>
   );
 };
