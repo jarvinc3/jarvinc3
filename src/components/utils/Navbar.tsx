35e02b13976ee1c0bb98c81411151c6dc44385a5
jarvinc3 <jarvinct3@gmail.com>
2024-08-08 08:52:49 -0400

    REMOVE ALL

--- src/components/utils/Navbar.tsx
+++ src/components/utils/Navbar.tsx
@@ -1,31 +1,8 @@
-interface NavbarProps {
-  setView: (view: string) => void;
-  view: string;
-}
 
-export default function Navbar({ setView, view }: NavbarProps) {
+export default function Navbar() {
   return (
-    <ul className="flex items-center justify-between gap-5 xl:flex-col">
-      <li onClick={() => setView("about")} className={`flex cursor-pointer transition-all duration-300 flex-col items-center px-4 py-2 rounded-md ${view === "about" ? "bg-[#9addee] scale-[1.05]" : "bg-slate-100 hover:bg-[#c2e4e5] hover:scale-[1.05]"}`}>
-        <img className="w-6 xl:w-8" src="/svg/user.svg" alt="" />
-        <span className="hidden xl:block">About</span>
-      </li>
-
-      <li onClick={() => setView("resume")} className={`flex cursor-pointer transition-all duration-300 flex-col items-center px-3 py-2 rounded-md ${view === "resume" ? "bg-[#9addee] scale-[1.05]" : "bg-slate-100 hover:bg-[#c2e4e5] hover:scale-[1.05]"}`}>
-        <img className="w-6 xl:w-8" src="/svg/resume.svg" alt="" />
-        <span className="hidden xl:block">Resume</span>
-      </li>
-
-      <li onClick={() => setView("works")} className={`flex cursor-pointer transition-all duration-300 flex-col items-center px-4 py-2 rounded-md ${view === "works" ? "bg-[#9addee] scale-[1.05]" : "bg-slate-100 hover:bg-[#c2e4e5] hover:scale-[1.05]"}`}>
-        <img className="w-6 xl:w-8" src="/svg/work.svg" alt="" />
-        <span className="hidden xl:block">Works</span>
-      </li>
-
-      <li onClick={() => setView("contact")} className={`flex cursor-pointer transition-all duration-300 flex-col items-center px-3 py-2 rounded-md ${view === "contact" ? "bg-[#9addee] scale-[1.05]" : "bg-slate-100 hover:bg-[#c2e4e5] hover:scale-[1.05]"}`}>
-        <img className="w-6 xl:w-8" src="/svg/contact.svg" alt="" />
-        <span className="hidden xl:block">Contact</span>
-      </li>
-
-    </ul>
+    <div>
+      
+    </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
-import About from "../components/common/About"
-import MainLayout from "../components/layout/MainLayout"
-import Works from "../components/common/Works";
-import Resume from "../components/common/Resume";
-import Contact from "../components/common/Contact";
+import Navbar from "../components/utils/Navbar"
 
 export const Home = () => {
-   const [view, setView] = useState<string>("about");
 
    return (
-      <MainLayout view={view} setView={setView}>
-         <div>
-            {view === "about" && <About />}
-            {view === "works" && <Works />}
-            {view === "resume" && <Resume />}
-            {view === "contact" && <Contact />}
-         </div>
-      </MainLayout>
+      <div className="bg-[#38393c] w-screen h-screen flex justify-center items-start p-20">
+         <Navbar />
+      </div>
    )
 }
