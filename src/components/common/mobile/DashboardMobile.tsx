9e1eb27f74964826562bb185a2da219f59e2acbf
jarvinc3 <jarvinct3@gmail.com>
2024-09-17 17:42:54 -0400

    mobile view message

--- src/components/common/mobile/DashboardMobile.tsx
+++ src/components/common/mobile/DashboardMobile.tsx
@@ -2,19 +2,8 @@
 export const DashboardMobile: React.FC = () => {
 
    return (
-      <div className="relative w-full max-w-sm mx-auto">
-         {/* Triángulo con imagen */}
-         <div className="relative w-full h-full overflow-hidden">
-            <div className="absolute inset-0 origin-bottom-right transform -rotate-45 bg-purple-600"></div>
-            <img
-               src="/portafolio.png"
-               alt="Profile"
-               className="relative z-10 object-cover w-full h-full"
-               style={{
-                  clipPath: "polygon(0 0, 100% 0, 0 100%)",
-               }}
-            />
-         </div>
+      <div className="relative flex items-center justify-center w-full h-full">
+         <p>This section is under construction</p>
       </div>
    );
 };
